85cf4374f15dd6e229479508d2a1fb70cdb2d878
nicola.defiorenze@ennova-research.com <nicola.defiorenze@ennova-research.com>
2017-02-23 12:36:54 +0100

    expose android sdk in upsource images

--- generator.sh
+++ generator.sh
@@ -2,12 +2,13 @@
 
 function man() {
   usage="Usage: $(basename "$0") [options... ]
-Program to generate the right folder structure && docker-compose.yml files to Ennova jetbreans products instnces
+Script to generate the right folder structure && docker-compose.yml files for JetBreans products instances.
 
     -h, --help    show this help text
     -t, --team    specify the team name
     -l, --tool    specify the tool (upsource, hub, youtrack)
     -p, --port    specify the port
+    -n, --network specify the host network interface
     -v, --version specify the tool version
     -b, --build   specify the tool build
 
@@ -40,6 +41,10 @@ else
           ;;
           -p|--port)
             PORT="$2"
+            shift
+          ;;
+          -n|--network)
+            NETWORK="$2"
             shift
           ;;
           -v|--version)
@@ -75,6 +80,12 @@ if [ -z "${BUILD}" ]; then
     echo "BUILD is unset";
     exit 1
 fi
+if [ -z "${NETWORK}" ]; then
+    echo "NETWORK is unset";
+    exit 1
+fi
+
+
 
 mkdir -p "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/docker
 mkdir -p "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/volume/"${VERSION}"."${BUILD}"
@@ -83,7 +94,7 @@ echo directory "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker" created
 echo directory "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"volume"/"${VERSION}"."${BUILD}" created
 mkdir -p "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/volume/"${VERSION}"."${BUILD}"/{"backups","data","log","tmp"}
 
-IP_HOST=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+IP_HOST=$(ifconfig "${NETWORK}" | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
 
 case "${TOOL}" in
@@ -92,6 +103,24 @@ case "${TOOL}" in
   ;;
   upsource)
     DOCKER_COMPOSE_FILE_NAME="d_compose_upsource.yml"
+
+    #Android home
+    EXPORT_ANDROID_HOME=0
+
+    if [ -z "${ANDROID_HOME}" ]; then
+        echo "ANDROID_HOME not setted";
+    else
+        echo "ANDROID_HOME found.";
+        while true; do
+        read -p "Do you want to export expose the android SDK to Upsource? y/n" yn
+        case $yn in
+            [Yy]* ) EXPORT_ANDROID_HOME=1; break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+done
+    fi
+
   ;;
 esac
 
@@ -103,8 +132,16 @@ sed -e s/=serviceName=/"${TOOL}"/g \
     -e s/=portContainer=/"${PORT}"/g \
     -e s/=ipHost=/"${IP_HOST}"/g  <  dockerFileTemplates/"${DOCKER_COMPOSE_FILE_NAME}" > "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker/docker-compose.yml"
 
+if [ ${EXPORT_ANDROID_HOME} -eq 1 ]; then
+    sed -e s/=exportAndroidHome=/""/g \
+        -e s/=hostAndroidHome=/"${ANDROID_HOME}"/g  <  "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker/docker-compose.yml" > "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker/docker-compose.yml"
+else
+    sed -e s/=exportAndroidHome=/"#"/g \
+        -e s/=hostAndroidHome=/"null"/g  <  "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker/docker-compose.yml" > "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker/docker-compose.yml"
+fi
+
 cp dockerFileTemplates/"${TOOL}"/Dockerfile  "${TEAM}"/"${TOOL}"/"${VERSION}"."${BUILD}"/"docker/Dockerfile"
 
-tree "${TEAM}"
+tree -L 3 -d "${TEAM}"
 
 echo "now exec: 'sudo docker-compose -f ${TEAM}/${TOOL}/${VERSION}.${BUILD}/docker/docker-compose.yml up' "
